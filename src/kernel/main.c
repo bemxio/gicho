e6cd931854fddb78a88f96ea9c3d5d592ce1d702
bemxio <bemxiov@protonmail.com>
2024-08-30 23:09:12 +0200

    create an echo command

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -12,7 +12,7 @@ void vidmodset(char mode) {
     );
 }
 
-int main() {
+void main() {
     char input[127];
     char* token;
 
@@ -30,7 +30,15 @@ int main() {
         if (strcmp(token, "help") == 0) {
             puts("Available commands:\r\n");
             puts("  help - Display this message.\r\n");
-            //puts("  echo - Echo the input.\r\n");
+            puts("  echo - Echo the input.\r\n");
+        } else if (strcmp(token, "echo") == 0) {
+            while ((token = strtok(NULL, " ")) != NULL) {
+                puts(token);
+                putchar(' ');
+            }
+
+            putchar('\r');
+            putchar('\n');
         } else if (input[0] == '\0') {
             continue;
         } else {
