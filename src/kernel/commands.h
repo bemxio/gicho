9b874d7dde3f36ca930c869ad93e16c13ff08268
bemxio <bemxiov@protonmail.com>
2024-09-08 09:31:04 +0200

    put all commands into a seperate file

---
+++ src/kernel/commands.h
@@ -0,0 +1,13 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+void help();
+void echo(char* token);
+void clear();
+void peek(char* token);
+void poke(char* token);
+void _int(char* token);
+void read(char* token);
+void write(char* token);
+
+#endif
