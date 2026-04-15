956b2e755e5bb709230f04587502214f47332e0d
bemxio <bemxiov@protonmail.com>
2026-04-15 19:09:26 +0200

    implement getchar and gets

--- src/kernel/include/io.h
+++ src/kernel/include/io.h
@@ -1,6 +1,11 @@
 #ifndef IO_H
 #define IO_H
 
+#include <stddef.h>
+
+char getchar();
+char* gets(char* buf);
+
 void putchar(char c);
 void puts(const char* s);
 
--- src/kernel/io.c
+++ src/kernel/io.c
@@ -1,5 +1,51 @@
 #include "io.h"
 
+char getchar() {
+    char c;
+
+    __asm__ (
+        "movb $0x00, %%ah\n"
+        "int $0x16\n"
+        "movb %%al, %0\n"
+        : "=r" (c)
+        :: "ah", "al"
+    );
+
+    return c;
+}
+
+char* gets(char* buf) {
+    char c;
+    size_t i = 0;
+
+    while ((c = getchar()) != '\r') {
+        if (c == '\b') {
+            if (i == 0)
+                continue;
+
+            buf[--i] = '\0';
+
+            putchar('\b');
+            putchar(' ');
+            putchar('\b');
+
+            continue;
+        }
+
+        buf[i] = c;
+        putchar(c);
+
+        i++;
+    }
+
+    buf[i] = '\0';
+
+    putchar('\r');
+    putchar('\n');
+
+    return buf;
+}
+
 void putchar(char c) {
     __asm__ (
         "movb $0x0e, %%ah\n"
